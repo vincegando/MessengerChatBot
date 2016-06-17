bbc2d59d2082c148eec7ad21e45988d385930081
Vince Gandolfo <vgandolfo@umail.ucsb.edu>
2016-06-17 00:11:39 -0700

    success

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 
-// module.exports = {
-//   Logger: require('./lib/logger.js').Logger,
-//   logLevels: require('./lib/logger.js').logLevels,
-//   Wit: require('./lib/wit.js').Wit,
-// }
-
 app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
@@ -21,7 +15,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send('Hello')
 })
 
 // for Facebook verification
@@ -104,58 +98,9 @@ function flip(sender) {
     })
 }
 
-function sendGenericMessage(sender) {
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "First card",
-                    "subtitle": "Element #1 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.messenger.com",
-                        "title": "web url"
-                    }, {
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for first element in a generic bubble",
-                    }],
-                }, {
-                    "title": "Second card",
-                    "subtitle": "Element #2 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
-                    }],
-                }]
-            }
-        }
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
 
 
-
-// Spin up the server
+// Start the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
